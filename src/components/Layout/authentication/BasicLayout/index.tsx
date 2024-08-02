4187302ffc9075e50bb6d89976146e25c53d10fb
hlanh012 <huynhlananh0411@gmail.com>
2024-08-02 14:56:39 +0700

    initial checker repo

--- src/components/Layout/authentication/BasicLayout/index.tsx
+++ src/components/Layout/authentication/BasicLayout/index.tsx
@@ -24,11 +24,6 @@ export type AuthProps = AuthPageProps<LayoutProps, CardProps, FormProps> & {
     title?: React.ReactNode;
 };
 
-/**
- * **refine** has a default auth page form served on the `/login` route when the `authProvider` configuration is provided.
- *
- * @see {@link https://refine.dev/docs/api-reference/antd/components/antd-auth-page/} for more details.
- */
 export const AuthPage: React.FC<AuthProps> = (props) => {
     const { type } = props;
     const { t } = useTranslation();
